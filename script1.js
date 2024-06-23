b28cb6d744563edda4cf28f9982701ffce2a5cb3
RAJ-HACKER2 <alirejuyan5@gmail.com>
2024-06-23 09:03:53 -0700

    Delete script1.js

--- script1.js
+++
@@ -1,1 +0,0 @@
-	document.write('<a href="https://www.freevisitorcounters.com/en/home/stats/id/1195722" target="_blank"><br><img src="https://www.freevisitorcounters.com/en/counter/render/1195722/t/3" border="0" class="counterimg"></a>');
